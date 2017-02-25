7336501eea6d025d0ad3661634a7fcaf1a43a244
Kevin Dong <dong.kevin1@gmail.com>
2017-02-25 17:39:32 -0500

    Add support for creating account, login, logout, and session manager

--- README.md
+++ README.md
@@ -9,9 +9,7 @@ This repo consists of the Web portion of the Chatr project. For details on what 
 3. Enter the newly cloned repo: `cd chatr-webserver`.
 4. Change to the `dev` branch: `git checkout dev`.
 5. Install all packages: `npm install`.
-6. Start Node.js server via nodemon: `npm run start:dev`.
-
-Note 1: The `npm run start:dev` command will automatically restart the local development server anytime it detects a file has changed. Anytime an uncaught exception gets thrown/a stack trace is printed, it'll appear in the terminal that you ran the command on.
+6. Start Node.js server: `npm start`.
 
 # Instructions for deploying to the dev instance on Heroku
 Just push your changes to the `dev` branch on Github. Heroku will detect a change has occured and automatically rebuild and deploy. Consequently, don't commit and push until you're done with your local changes.
--- server.js
+++ server.js
@@ -1,15 +1,76 @@
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const compression = require('compression');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('express-flash');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const dotenv = require('dotenv');
+const passport = require('passport');
 
-const port = process.env.PORT || 8080;
+// Load environment variables from .env file
+dotenv.load();
+
+// Controllers
+const HomeController = require('./controllers/home');
+const userController = require('./controllers/user');
+const contactController = require('./controllers/contact');
+
+// Passport OAuth strategies
+require('./config/passport');
+
 const app = express();
 
-// Imports routes from controller
-require('./server/routes')(app);
-app.get('*', (req, res) => {
-	res.send('Hello world from the dev branch!');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(methodOverride('_method'));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user ? req.user.toJSON() : null;
+  next();
+});
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', HomeController.index);
+app.get('/contact', contactController.contactGet);
+app.post('/contact', contactController.contactPost);
+app.get('/account', userController.ensureAuthenticated, userController.accountGet);
+app.put('/account', userController.ensureAuthenticated, userController.accountPut);
+app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
+app.get('/signup', userController.signupGet);
+app.post('/signup', userController.signupPost);
+app.get('/login', userController.loginGet);
+app.post('/login', userController.loginPost);
+app.get('/forgot', userController.forgotGet);
+app.post('/forgot', userController.forgotPost);
+app.get('/reset/:token', userController.resetGet);
+app.post('/reset/:token', userController.resetPost);
+app.get('/logout', userController.logout);
+app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
+
+// Production error handler
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.sendStatus(err.status || 500);
+  });
+}
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
-});
 module.exports = app;
