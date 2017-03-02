375094eeb6fc222e0f4ddddc7973542d092797af
patel445 <patel445@purdue.edu>
2017-03-01 23:14:58 -0500

    Jade to pug

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ require('./config/passport');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 3000);
 app.use("*", function (req, res, next) {
   if (process.env.NODE_ENV === 'production')
