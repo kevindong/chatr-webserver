cdfea3b42c6efb91bc7fe14e8b3ef73ed1a6b5a7
Scott Mangiapane <scottmangiapane@gmail.com>
2017-03-05 15:08:48 -0500

    Made admin console dynamic

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,18 @@
-/**
- * GET /
- */
-exports.index = function(req, res) {
-	res.render('admin', {
-		title: 'Admin',
-	});
+const https = require('https');
+
+exports.index = function (req, res) {
+    (new Promise(function (resolve, reject) {
+        https.get(`https://chatr-apiserver-dev.herokuapp.com/modules/pending`, (res) => {
+            res.on('data', (d) => {
+                resolve(d);
+            });
+        }).on('error', (err) => {
+            reject(err);
+        });
+    })).then(function (data) {
+        res.render('admin', {
+            title: 'Admin',
+            pendingModules: JSON.parse(data),
+        });
+    });
 };
