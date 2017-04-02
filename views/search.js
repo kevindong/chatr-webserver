df4a4735e959b228b754690aa0f9d714cd1a34bc
Jordan Pitlor <jordan@jordanpitlor.com>
2017-04-02 13:04:40 -0400

    searching calls the api, but i cant test it because the server is crashing for some reason

--- views/search.js
+++ views/search.js
@@ -11,15 +11,23 @@ request.onload = function() {
 		// Success!
 		const data = JSON.parse(request.responseText);
 		data.forEach((e) => {
-			// Create an li
+			const li = document.createElement('li');
+			li.innerHTML = `<a href=${e.id}>${e.name}</a>`;
+			document.getElementById('results').appendChild(li);
 		});
 	} else {
 		// We reached our target server, but it returned an error
+		const li = document.createElement('li');
+		li.innerHTML = 'There was an error!';
+		document.getElementById('results').appendChild(li);
 	}
 };
 
 request.onerror = function() {
 	// There was a connection error of some sort
+	const li = document.createElement('li');
+	li.innerHTML = 'There was an error!';
+	document.getElementById('results').appendChild(li);
 };
 
 request.send();
