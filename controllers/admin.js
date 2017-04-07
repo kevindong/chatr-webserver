d7724ef0cbf12e243181e0efe2ccc0888b490792
Kevin Dong <dong.kevin1@gmail.com>
2017-04-07 13:18:56 -0400

    Add admin page to manage user

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,6 @@
 const https = require('https');
+const request = require('request');
+const User = require('../models/User');
 
 exports.index = function(req, res) {
 	(new Promise((resolve, reject) => {
@@ -36,5 +38,52 @@ exports.index = function(req, res) {
 				});
 			});
 		});
+	});
+};
+
+exports.users = function(req, res) {
+	new Promise((resolve, reject) => {
+		request(`http://${process.env.API_SERVER}/users/get`, (error, response, body) => {
+			if (error) {
+				reject(error);
+			}
+			resolve(JSON.parse(body));
+		});
+	}).then((data) => {
+		console.log(data);
+		res.render('admin/manage_users', {
+			title: 'Manage Users',
+			users: data,
+		});
+	}).catch((e) => {
+		console.error(e);
+		res.status(500).send(e);
+	});
+};
+
+exports.adminAccountDelete = function(req, res, next) {
+	const options = {
+		hostname: process.env.API_SERVER,
+		port: 443,
+		path: '/users/delete',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+  		},
+	};
+	const request = https.request(options, (a) => {
+		console.log(`Status: ${  a.statusCode}`);
+		a.setEncoding('utf8');
+		a.on('data', (body) => {
+			console.log(`Body: ${  body}`);
+		});
+	});
+	request.on('error', (e) => {
+		console.log(`problem with request: ${  e.message}`);
+	});
+	request.write(`{"email": "${req.params.email}"}`);
+	request.end();
+	new User().where("email", req.params.email).destroy().then((user) => {
+		res.redirect('/');
 	});
 };
