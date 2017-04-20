0fadb308cb674198958d837affdef7e63ec760ed
Michael Bilstein <micha3lbilstein@gmail.com>
2017-04-20 19:35:10 -0400

    stringy

--- controllers/modules.js
+++ controllers/modules.js
@@ -15,7 +15,7 @@ function getModules(userId) {
 
 function uploadModule(req, res) {
 	console.log(req.user.email);
-	console.log(req.user);
+	console.log(JSON.stringify(req.user));
 	//First get the API server's info on this user.
 	rp.get(`https://${process.env.API_SERVER}/users/get/${req.user.email}/email`)
 		.then((response) => {
