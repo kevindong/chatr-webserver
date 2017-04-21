aab435e3e1410a2ccc06e83c71ba0f49b6a365cc
Michael Bilstein <micha3lbilstein@gmail.com>
2017-04-21 16:32:34 -0400

    Fix?

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 app.set('port', process.env.PORT || 3000);
+/*
 app.use('*', (req, res, next) => {
     if (process.env.NODE_ENV === 'production') {
         if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -40,6 +41,7 @@ app.use('*', (req, res, next) => {
         return next();
     }
 });
+*/
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
