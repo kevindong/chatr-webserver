3e0397ddc72cc6a57d22ce058d0f0945bc8880d5
Scott Mangiapane <scottmangiapane@gmail.com>
2017-04-26 20:19:46 -0400

    Only admins can access admin console

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,52 +3,72 @@ const request = require('request');
 const User = require('../models/User');
 
 exports.index = function (req, res) {
+	if (!req.user) {
+		return res.redirect('/');
+	}
 	new Promise((resolve, reject) => {
-		request(`https://${process.env.API_SERVER}/modules/pending`, (error, response, body) => {
+		request(`https://${process.env.API_SERVER}/users/get/${req.user.attributes.email}/email`, (error, response, body) => {
 			if (error) {
 				reject(error);
 			}
 			resolve(JSON.parse(body));
 		});
-	}).then((pendingModules) => {
-		new Promise((resolve, reject) => {
-			request(`http://${process.env.API_SERVER}/modules/get`, (error, response, body) => {
-				if (error) {
-					reject(error);
-				}
-				resolve(JSON.parse(body));
-			});
-		}).then((modules) => {
+	}).then((user) => {
+		console.log("User #" + user.id + " is an admin: " + user.isAdmin);
+		if (user.isAdmin) {
 			new Promise((resolve, reject) => {
-				request(`http://${process.env.API_SERVER}/users/get`, (error, response, body) => {
+				request(`https://${process.env.API_SERVER}/modules/pending`, (error, response, body) => {
 					if (error) {
 						reject(error);
 					}
 					resolve(JSON.parse(body));
 				});
-			}).then((users) => {
-				res.render('admin', {
-					title: 'Admin',
-					pendingModules: pendingModules,
-					modules: modules,
-					users: users,
-					server: `${process.env.API_SERVER}`,
+			}).then((pendingModules) => {
+				new Promise((resolve, reject) => {
+					request(`http://${process.env.API_SERVER}/modules/get`, (error, response, body) => {
+						if (error) {
+							reject(error);
+						}
+						resolve(JSON.parse(body));
+					});
+				}).then((modules) => {
+					new Promise((resolve, reject) => {
+						request(`http://${process.env.API_SERVER}/users/get`, (error, response, body) => {
+							if (error) {
+								reject(error);
+							}
+							resolve(JSON.parse(body));
+						});
+					}).then((users) => {
+						res.render('admin', {
+							title: 'Admin',
+							pendingModules: pendingModules,
+							modules: modules,
+							users: users,
+							server: `${process.env.API_SERVER}`,
+						});
+					}).catch((e) => {
+						console.error(e);
+						res.status(500).send(e);
+					});
+				}).catch((e) => {
+					console.error(e);
+					res.status(500).send(e);
 				});
 			}).catch((e) => {
 				console.error(e);
 				res.status(500).send(e);
 			});
-		}).catch((e) => {
-			console.error(e);
-			res.status(500).send(e);
-		});
+		} else {
+			res.status(400).send("ERROR: You are not an admin.");
+		}
 	}).catch((e) => {
 		console.error(e);
 		res.status(500).send(e);
 	});
 };
 
-exports.adminAccountDelete = function(req, res, next) {
+exports.adminAccountDelete = function (req, res, next) {
 	const options = {
 		hostname: process.env.API_SERVER,
 		port: 443,
@@ -56,17 +76,17 @@ exports.adminAccountDelete = function(req, res, next) {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
-  		},
+		},
 	};
 	const request = https.request(options, (a) => {
-		console.log(`Status: ${  a.statusCode}`);
+		console.log(`Status: ${a.statusCode}`);
 		a.setEncoding('utf8');
 		a.on('data', (body) => {
-			console.log(`Body: ${  body}`);
+			console.log(`Body: ${body}`);
 		});
 	});
 	request.on('error', (e) => {
-		console.log(`problem with request: ${  e.message}`);
+		console.log(`problem with request: ${e.message}`);
 	});
 	request.write(`{"email": "${req.params.email}"}`);
 	request.end();
